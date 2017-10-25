b076f8b8ad1242ea95ca7a393b1246e60135571f
DipanshuShora <deepanshushora478@gmail.com>
2017-10-25 20:22:27 +0530

    css less sass loaders are loaded

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import '../css/style.css';
+import '../less/style.less';
+import '../scss/style.scss';
 class ReactComponents extends React.Component{
     render(){
         return(
-            <h1>Hello World</h1>
+            <div>
+                <h1 className="heading-1">Hello World from : css-loader</h1>
+                <h1 className="heading-2">Hello World from : less-loader</h1>
+                <h1 className="heading-3">Hello World from : scss-loader</h1>
+            </div>
         );
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-var path = require('path');
-var SRC = path.resolve(__dirname,'src');
-var DIST = path.resolve(__dirname,'dist');
+const path = require('path');
+const SRC = path.resolve(__dirname,'src');
+const DIST = path.resolve(__dirname,'dist');
 
 module.exports = {
     entry: SRC + '/app/index.js',
@@ -10,12 +10,35 @@ module.exports = {
         publicPath: '/app/'
     },
     module:{
-        loaders:[
+        rules:[
             {
                 test: /\.js$/,
                 include: SRC,
-                loader: 'babel-loader',
+                use: 'babel-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.less/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
             }
         ]
     }
