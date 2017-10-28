d2b4e656c71f16a1db9c889de857a4cdc64a81fe
DipanshuShora <deepanshushora478@gmail.com>
2017-10-29 02:01:21 +0530

    loader changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const SRC = path.resolve(__dirname,'src');
 const DIST = path.resolve(__dirname,'dist');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractCSS = new ExtractTextPlugin('../css/style-css.css');
+const extractLESS = new ExtractTextPlugin('../css/style-less.css');
+const extractSASS = new ExtractTextPlugin('../css/style-sass.css');
 
 module.exports = {
     entry: SRC + '/app/index.js',
@@ -12,8 +16,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname,'dist'),
         compress:true,
-        open:true,
-        port:9000
+        open:true
     },
     module:{
         rules:[
@@ -25,27 +28,38 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use:[
-                    'style-loader',
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use:[
                     'css-loader'
-                ]
+                  ]
+                })
             },
             {
                 test: /\.scss$/,
-                use:[
-                    'style-loader',
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: [
                     'css-loader',
                     'sass-loader'
-                ]
+                  ]
+                })
             },
             {
-                test: /\.less/,
-                use:[
-                    'style-loader',
+                test: /\.less$/i,
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: [
                     'css-loader',
                     'less-loader'
-                ]
+                  ]
+                })
             }
         ]
-    }
+    },
+    plugins: [
+      extractCSS,
+      extractSASS,
+      extractLESS
+    ]
 };
